d84acd151414ee1344e0999fb7b3e24a8072394e
1 <v_derkach@inbox.ru>
2024-06-21 13:16:02 +0500

    2

--- classcollection.py
+++ classcollection.py
@@ -12,3 +12,14 @@ class Car:
 class Nissan(Car, Vehicle):
     def __init__(self,vehicle_type,price,horse_power):
         super().__init__()
+        self.vehicle_type= vehicle_type
+        self.price=  price
+        self.horse_power= horse_power
+    def __str__(self):
+        return f'{self.vehicle_type}, {self.price}'
+    def horse_powers(self):
+        return f'Базовая мощность: {super().horse_power} для Nissan: {self.horse_power}'
+
+if __name__=='__main__':
+    Auto= Nissan('Karina',50,25)
+    print(Auto)
